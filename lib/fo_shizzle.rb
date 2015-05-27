ab3d64f59937e1e0d0558d83db99116224ef7cca
stephen <lonelytreesl@yahoo.com>
2015-05-27 16:52:12 -0700

    add spec and code to fo_shizzle.rb to not change  letters that = s to z if s starts a word, all specs passing.

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -1,14 +1,32 @@
 class String
   define_method(:fo_shizzle) do
-    words = []
-    letters = self.split("")
-    letters.each do |letter|
-      if letter.==("s")
-        words.push("z")
-      else
-        words.push(letter)
+    new_words = []
+    old_words = self.split(" ")
+
+    old_words.each do |old_word|
+      letters = old_word.split("")
+      index = 0
+
+      new_letters = []
+      letters.each() do |letter|
+
+        if letter.==("s") && index.==(0)
+          new_letters.push(letter)
+        elsif letter.==("s")
+          new_letters.push("z")
+        else
+          new_letters.push(letter)
+        end
+
+        index = index + 1
       end
+
+      # new_words.push(" ")
+      new_words.push(new_letters.join(""))
+
     end
-    words.join()
+
+    # new_words.pop()
+    new_words.join(" ")
   end
 end
--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -1,13 +1,17 @@
 require('rspec')
 require('fo_shizzle')
+require('pry')
 
 describe('String#fo_shizzle') do
 
   it("will change all letter s' to z's") do
-    expect('supers'.fo_shizzle).to(eq("zuperz"))
+    expect('dupers'.fo_shizzle).to(eq("duperz"))
+  end
+  it('changes letter s to z unless it is the first letter of the string') do
+    expect("supers".fo_shizzle()).to(eq("superz"))
   end
 
-#   it('changes letter s to z unless it is the first letter of a word in a string of multiple words') do
-#     expect("she has super blonde hairs".fo_shizzle()).to(eq("she haz super blonde hairz"))
-#   end
+  it('changes letter s to z unless it is the first letter of a word in a string of multiple words') do
+    expect("she has super blonde hairs".fo_shizzle()).to(eq("she haz super blonde hairz"))
+  end
 end
